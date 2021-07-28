948a981630dabcbffe31c4719950d983b38189fb
jgonzalez.benjamin <jgonzalez.benjamin@gmail.com>
2021-07-28 13:40:11 -0300

    Se acomodo la pantalla del file-manager, se quitaron items, y se cambio las leyendas de cada uno de los folders/files

--- src/app/mock-api/apps/file-manager/data.ts
+++ src/app/mock-api/apps/file-manager/data.ts
@@ -3,43 +3,43 @@ export const items = [
     {
         id         : 'cd6897cb-acfd-4016-8b53-3f66a5b5fc68',
         folderId   : null,
-        name       : 'Personal',
+        name       : '07-2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'April 24, 2018',
         modifiedAt : 'April 24, 2018',
         size       : '87 MB',
         type       : 'folder',
-        contents   : '57 files',
+        contents   : '1 files',
         description: 'Personal documents such as insurance policies, tax papers and etc.'
     },
     {
         id         : '6da8747f-b474-4c9a-9eba-5ef212285500',
         folderId   : null,
-        name       : 'Photos',
+        name       : '06-2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'November 01, 2021',
         modifiedAt : 'November 01, 2021',
         size       : '3015 MB',
         type       : 'folder',
-        contents   : '907 files',
+        contents   : '2 files',
         description: 'Personal photos; selfies, family, vacation and etc.'
     },
     {
         id         : 'ed58add1-45a7-41db-887d-3ca7ee7f2719',
         folderId   : null,
-        name       : 'Work',
+        name       : '05-2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'May 8, 2020',
         modifiedAt : 'May 8, 2020',
         size       : '14 MB',
         type       : 'folder',
-        contents   : '24 files',
+        contents   : '2 files',
         description: 'Work related files, mainly documents and paychecks.'
     },
     {
         id         : '5cb66e32-d1ac-4b9a-8c34-5991ce25add2',
         folderId   : null,
-        name       : 'Contract #123',
+        name       : 'Resúmen-07/2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'January 14, 2021',
         modifiedAt : 'January 14, 2021',
@@ -51,7 +51,7 @@ export const items = [
     {
         id         : '3ffc3d84-8f2d-4929-903a-ef6fc21657a7',
         folderId   : null,
-        name       : 'Estimated budget',
+        name       : 'Resúmen-06/2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'December 14, 2020',
         modifiedAt : 'December 14, 2020',
@@ -63,7 +63,7 @@ export const items = [
     {
         id         : '157adb9a-14f8-4559-ac93-8be893c9f80a',
         folderId   : null,
-        name       : 'DMCA notice #42',
+        name       : 'Resúmen-05/2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'May 8, 2021',
         modifiedAt : 'May 8, 2021',
@@ -75,7 +75,7 @@ export const items = [
     {
         id         : '4f64597a-df7e-461c-ad60-f33e5f7e0747',
         folderId   : null,
-        name       : 'Invoices',
+        name       : 'Resúmen-04/2021',
         createdBy  : 'Brian Hughes',
         createdAt  : 'January 12, 2020',
         modifiedAt : 'January 12, 2020',
@@ -83,8 +83,8 @@ export const items = [
         type       : 'PDF',
         contents   : null,
         description: null
-    },
-    {
+    }
+   /*  ,{
         id         : 'e445c445-57b2-4476-8c62-b068e3774b8e',
         folderId   : null,
         name       : 'Crash logs',
@@ -253,5 +253,5 @@ export const items = [
         type       : 'XLS',
         contents   : null,
         description: null
-    }
+    } */
 ];
