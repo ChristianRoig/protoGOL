150fe0b2744de42077a76474500e89c1818d3212
benja-gonzalez <88004298+benja-gonzalez@users.noreply.github.com>
2021-09-03 11:12:44 -0300

    se cambiaron todos los logos de fuse por los de la anonina y tdm-la

--- src/app/mock-api/common/user/data.ts
+++ src/app/mock-api/common/user/data.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export const user = {
     id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
-    name  : 'Jorge Boria',
-    email : 'j.boria@gmail.com',
+    name  : 'Benja Dev',
+    email : 'j.gonzalez@sieteideas.com.ar',
     avatar: 'assets/images/avatars/brian-hughes.jpg',
     status: 'online'
 };
