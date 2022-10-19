da816e7b1e8910ef399ca88babae08105e1318b6
Christian Roig <ch.roig@gmail.com>
2022-10-18 21:53:27 -0300

    data sp

---
+++ src/app/mock-api/apps/contacts/min-data-sp.ts
@@ -0,0 +1,220 @@
+/* eslint-disable */
+export const contacts = [
+    {
+        id          : 'cd5fa417-b667-482d-b208-798d9da3213c',
+        avatar      : 'assets/images/avatars/male-01.jpg',
+        background  : 'assets/images/cards/14-640x480.jpg',
+        name        : 'Martin Garcia',
+        emails      : [
+            {
+                email: 'm.garcia@gmail.com',
+                label: 'Personal'
+            },
+            {
+                email: 'martin-g@7ideas.com',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'bs',
+                phoneNumber: '4531 2468',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'bs',
+                phoneNumber: '3470 2693',
+                label      : 'Work'
+            }
+        ],
+        title       : 'Ingeniero',
+        company     : '7ideas',
+        birthday    : '1975-01-10T12:00:00.000Z',
+        address     : 'Av. Independencia 865, (7000) Tandil',
+        notes       : '<p>Do incididunt cillum duis eu pariatu pariati voluptate dolor minim enim ut eu cupidatat enim.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : 'beec5287-ed50-4504-858a-5dc3f8ce6935',
+        avatar      : null,
+        background  : null,
+        name        : 'Diana Molina',
+        emails      : [
+            {
+                email: 'dianamolina@mail.us',
+                label: 'Personal'
+            },
+            {
+                email: 'molina.diana@laanonima.com',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'bs',
+                phoneNumber: '4537 3180',
+                label      : 'Mobile'
+            }
+        ],
+        title       : 'Analista',
+        company     : 'La Anonima',
+        birthday    : '1994-12-05T12:00:00.000Z',
+        address     : 'Av. Rivadavia 1422, (1003) CABA',
+        notes       : '<p>Consequat quis officia irure fugiat ex duis eu amet ex.</p><p>Irure est nisi dolorp veniam.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : '9d3f0e7f-dcbd-4e56-a5e8-87b8154e9edf',
+        avatar      : 'assets/images/avatars/male-02.jpg',
+        background  : 'assets/images/cards/15-640x480.jpg',
+        name        : 'Fabian Quinteros',
+        emails      : [
+            {
+                email: 'fquinteros@mail.com',
+                label: 'Personal'
+            },
+            {
+                email: 'fabquint@fava.net',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'bs',
+                phoneNumber: '5548 2862',
+                label      : 'Mobile'
+            }
+        ],
+        title       : 'Scrum Master',
+        company     : 'Grupo Fava',
+        birthday    : '1988-05-26T12:00:00.000Z',
+        address     : 'Av. Pedro Luro 1833, (7600) Mar del Plata',
+        notes       : '<p>Est amet in adipisicing velit.</p>',
+        tags        : [
+            'c31e9e5d-e0cb-4574-a13f-8a6ee5ff8309'
+        ]
+    },
+    {
+        id          : '42a5da95-5e6d-42fd-a09d-de755d123a47',
+        avatar      : 'assets/images/avatars/male-03.jpg',
+        background  : 'assets/images/cards/16-640x480.jpg',
+        name        : 'Martin Ceteri',
+        emails      : [
+            {
+                email: 'martin@mail.me',
+                label: 'Personal'
+            },
+            {
+                email: 'ceterim@cfa.info',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'co',
+                phoneNumber: '3484 3813',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'co',
+                phoneNumber: '2475 2789',
+                label      : 'Work'
+            }
+        ],
+        company     : 'CFA',
+        birthday    : '1968-08-13T12:00:00.000Z',
+        address     : 'Callao 435, (3400) Cordoba',
+        notes       : '<p>Dolore do ea nulla incididunt proident ea eiusmod nostrud esse.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : 'a7806ced-03f1-4197-8b30-00bdd463366b',
+        avatar      : 'assets/images/avatars/male-04.jpg',
+        background  : 'assets/images/cards/17-640x480.jpg',
+        name        : 'Daniela Gonzalez',
+        emails      : [
+            {
+                email: 'dani@mail.biz',
+                label: 'Personal'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'er',
+                phoneNumber: '1401 3980',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'er',
+                phoneNumber: '2546 2589',
+                label      : 'Work'
+            }
+        ],
+        title       : 'Soporte Técnico',
+        company     : '7ideas',
+        birthday    : '1983-12-22T12:00:00.000Z',
+        address     : 'Pinto 399, (7000) Tandil',
+        notes       : '<p>Id eiusmod deserunt incididunt ut duis.</p>',
+        tags        : [
+            '2026ce08-d08f-4b4f-9506-b10cdb5b104f'
+        ]
+    }
+];
+
+export const countries = [
+    {
+        id          : '53c77399-494e-49df-9e3a-587b536c033e',
+        iso         : 'ar',
+        name        : 'Argentina',
+        code        : '+54',
+        flagImagePos: '-1px -3282px'
+    },
+    {
+        id          : '4505ba35-afa5-47ef-a6c7-9b57f1dcd187',
+        iso         : 'au',
+        name        : 'Australia',
+        code        : '+61',
+        flagImagePos: '-1px -2279px'
+    },
+    {
+        id          : '57b3cd1f-d5d6-403b-8137-fbeeacaf136a',
+        iso         : 'at',
+        name        : 'Austria',
+        code        : '+43',
+        flagImagePos: '-1px -1735px'
+    },
+    {
+        id          : '571bf396-810b-4fc4-9ffc-c9e4db9d3bef',
+        iso         : 'bo',
+        name        : 'Bolivia',
+        code        : '+591',
+        flagImagePos: '-1px -2177px'
+    },
+    {
+        id          : '2dea0689-0548-400c-a58f-ebcd6373cd07',
+        iso         : 'br',
+        name        : 'Brazil',
+        code        : '+55',
+        flagImagePos: '-1px -1004px'
+    }
+];
+export const tags = [
+    {
+        id   : 'c31e9e5d-e0cb-4574-a13f-8a6ee5ff8309',
+        title: 'Trabajo'
+    },
+    {
+        id   : 'a8991c76-2fda-4bbd-a718-df13d6478847',
+        title: 'Amigos'
+    },
+    {
+        id   : '56ddbd47-4078-4ddd-8448-73c5e88d5f59',
+        title: 'Familia'
+    }
+];
---
+++ src/app/mock-api/apps/contacts/min-data.ts
@@ -0,0 +1,225 @@
+/* eslint-disable */
+export const contacts = [
+    {
+        id          : 'cd5fa417-b667-482d-b208-798d9da3213c',
+        avatar      : 'assets/images/avatars/male-01.jpg',
+        background  : 'assets/images/cards/14-640x480.jpg',
+        name        : 'Dejesus Michael',
+        emails      : [
+            {
+                email: 'dejesusmichael@mail.org',
+                label: 'Personal'
+            },
+            {
+                email: 'michael.dejesus@vitricomp.io',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'bs',
+                phoneNumber: '984 531 2468',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'bs',
+                phoneNumber: '806 470 2693',
+                label      : 'Work'
+            }
+        ],
+        title       : 'Track Service Worker',
+        company     : 'Vitricomp',
+        birthday    : '1975-01-10T12:00:00.000Z',
+        address     : '279 Independence Avenue, Calvary, Guam, PO4127',
+        notes       : '<p>Do incididunt cillum duis eu pariatu pariati voluptate dolor minim enim ut eu cupidatat enim.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : 'beec5287-ed50-4504-858a-5dc3f8ce6935',
+        avatar      : null,
+        background  : null,
+        name        : 'Dena Molina',
+        emails      : [
+            {
+                email: 'denamolina@mail.us',
+                label: 'Personal'
+            },
+            {
+                email: 'molina.dena@envire.tv',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'io',
+                phoneNumber: '934 537 3180',
+                label      : 'Mobile'
+            }
+        ],
+        title       : 'Weather Analyst',
+        company     : 'Envire',
+        birthday    : '1994-12-05T12:00:00.000Z',
+        address     : '856 Woodside Avenue, Alfarata, Iowa, PO4992',
+        notes       : '<p>Consequat quis officia irure fugiat ex duis eu amet ex.</p><p>Irure est nisi dolorp veniam.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : '9d3f0e7f-dcbd-4e56-a5e8-87b8154e9edf',
+        avatar      : 'assets/images/avatars/male-02.jpg',
+        background  : 'assets/images/cards/15-640x480.jpg',
+        name        : 'Bernard Langley',
+        emails      : [
+            {
+                email: 'bernardlangley@mail.com',
+                label: 'Personal'
+            },
+            {
+                email: 'langley.bernard@boilcat.name',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'md',
+                phoneNumber: '893 548 2862',
+                label      : 'Mobile'
+            }
+        ],
+        title       : 'Electromedical Equipment Technician',
+        company     : 'Boilcat',
+        birthday    : '1988-05-26T12:00:00.000Z',
+        address     : '943 Adler Place, Hamilton, South Dakota, PO5592',
+        notes       : '<p>Est amet in adipisicing velit.</p>',
+        tags        : [
+            'c31e9e5d-e0cb-4574-a13f-8a6ee5ff8309'
+        ]
+    },
+    {
+        id          : '42a5da95-5e6d-42fd-a09d-de755d123a47',
+        avatar      : 'assets/images/avatars/male-03.jpg',
+        background  : 'assets/images/cards/16-640x480.jpg',
+        name        : 'Mclaughlin Steele',
+        emails      : [
+            {
+                email: 'mclaughlinsteele@mail.me',
+                label: 'Personal'
+            },
+            {
+                email: 'steele.mclaughlin@accel.info',
+                label: 'Work'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'va',
+                phoneNumber: '830 484 3813',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'va',
+                phoneNumber: '999 475 2789',
+                label      : 'Work'
+            },
+            {
+                country    : 'va',
+                phoneNumber: '933 406 3598',
+                label      : 'Home'
+            }
+        ],
+        company     : 'Accel',
+        birthday    : '1968-08-13T12:00:00.000Z',
+        address     : '334 Sandford Street, Savage, Virgin Islands, PO1858',
+        notes       : '<p>Dolore do ea nulla incididunt proident ea eiusmod nostrud esse.</p>',
+        tags        : [
+            '56ddbd47-4078-4ddd-8448-73c5e88d5f59'
+        ]
+    },
+    {
+        id          : 'a7806ced-03f1-4197-8b30-00bdd463366b',
+        avatar      : 'assets/images/avatars/male-04.jpg',
+        background  : 'assets/images/cards/17-640x480.jpg',
+        name        : 'Marsh Cochran',
+        emails      : [
+            {
+                email: 'marshcochran@mail.biz',
+                label: 'Personal'
+            }
+        ],
+        phoneNumbers: [
+            {
+                country    : 'tz',
+                phoneNumber: '864 401 3980',
+                label      : 'Mobile'
+            },
+            {
+                country    : 'tz',
+                phoneNumber: '956 546 2589',
+                label      : 'Work'
+            }
+        ],
+        title       : 'Fundraising Director',
+        company     : 'Xsports',
+        birthday    : '1983-12-22T12:00:00.000Z',
+        address     : '487 Hamilton Walk, Bergoo, American Samoa, PO5616',
+        notes       : '<p>Id eiusmod deserunt incididunt ut duis.</p>',
+        tags        : [
+            '2026ce08-d08f-4b4f-9506-b10cdb5b104f'
+        ]
+    }
+];
+
+export const countries = [
+    {
+        id          : '53c77399-494e-49df-9e3a-587b536c033e',
+        iso         : 'ar',
+        name        : 'Argentina',
+        code        : '+54',
+        flagImagePos: '-1px -3282px'
+    },
+    {
+        id          : '4505ba35-afa5-47ef-a6c7-9b57f1dcd187',
+        iso         : 'au',
+        name        : 'Australia',
+        code        : '+61',
+        flagImagePos: '-1px -2279px'
+    },
+    {
+        id          : '57b3cd1f-d5d6-403b-8137-fbeeacaf136a',
+        iso         : 'at',
+        name        : 'Austria',
+        code        : '+43',
+        flagImagePos: '-1px -1735px'
+    },
+    {
+        id          : '571bf396-810b-4fc4-9ffc-c9e4db9d3bef',
+        iso         : 'bo',
+        name        : 'Bolivia',
+        code        : '+591',
+        flagImagePos: '-1px -2177px'
+    },
+    {
+        id          : '2dea0689-0548-400c-a58f-ebcd6373cd07',
+        iso         : 'br',
+        name        : 'Brazil',
+        code        : '+55',
+        flagImagePos: '-1px -1004px'
+    }
+];
+export const tags = [
+    {
+        id   : 'c31e9e5d-e0cb-4574-a13f-8a6ee5ff8309',
+        title: 'Work'
+    },
+    {
+        id   : 'a8991c76-2fda-4bbd-a718-df13d6478847',
+        title: 'Friend'
+    },
+    {
+        id   : '56ddbd47-4078-4ddd-8448-73c5e88d5f59',
+        title: 'Family'
+    }
+];
